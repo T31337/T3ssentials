d2e6db9151aab658c4abdb588178e43d29fc47ce
T31337 <T23N70N@gmail.com>
2019-03-11 00:23:34 -0500

    T31337's Essential MineCraft Plugin For Bukkit/Spigot Servers...

---
+++ src/main/java/com/github/t31337/t3ssentials/T3ssentials.java
@@ -0,0 +1,20 @@
+package com.github.t31337.t3ssentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class T3ssentials extends JavaPlugin
+{
+	
+	@Override
+	public void onEnable()
+	{
+		// Plugin startup logic
+		
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		// Plugin shutdown logic
+	}
+}
